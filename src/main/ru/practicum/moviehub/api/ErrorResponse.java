cb1bb185417a38e8baa96cb8758e0fb9e81d1567
aleksandra.bikbaeva <aleksandra.bikbaeva_axiomjdk.ru>
2026-06-30 21:38:21 +0300

    Исправить ошибки по стилю кода в ErrorResponse

--- src/main/ru/practicum/moviehub/api/ErrorResponse.java
+++ src/main/ru/practicum/moviehub/api/ErrorResponse.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class ErrorResponse {
     private final String error;
     private final List<String> details;
+
     public ErrorResponse(String error, List<String> details) {
         this.error = error;
         this.details = details;
